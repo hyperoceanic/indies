b7d2cde6dba32628fba8a76980b104f636e8c7d3
Mark Smith <mark.smith@hyperoceanic.com>
2020-08-05 18:05:20 +0100

    Added Indie model

---
+++ API/Model/Indie.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace API
+{
+    public class Indie
+    {
+        public DateTime CreatedDate { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+    }
+}
