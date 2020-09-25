272d1ef23230d421a47d2d338933f7d1c54ac005
Mark Smith <mark.smith@hyperoceanic.com>
2020-09-25 20:35:00 +0100

    M3

---
+++ Domain/Base.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace API
+{
+    public class Base
+    {
+        public int Id {get;set;}
+        public DateTime CreatedDate { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+    }
+}
